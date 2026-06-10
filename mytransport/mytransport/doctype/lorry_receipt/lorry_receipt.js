5d3d221ad36bc86d6d20800f44c8d5c0de317efb
mukesh339yahoo <mukeshsharma339@yahoo.com>
2026-06-10 13:20:34 +0530

    feat: Add Detention, RTO and Other charges to LR

--- mytransport/mytransport/doctype/lorry_receipt/lorry_receipt.js
+++ mytransport/mytransport/doctype/lorry_receipt/lorry_receipt.js
@@ -1,8 +1,57 @@
-// Copyright (c) 2026, Shaily Sharma and contributors
+// Copyright (c) 2026, Ridhira Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Lorry Receipt", {
-// 	refresh(frm) {
+frappe.ui.form.on("Lorry Receipt", {
+	setup: function(frm) {
+		// Calculate totals on setup if needed
+	},
 
-// 	},
-// });
+	basic_freight: function(frm) {
+		calculate_total_amount(frm);
+	},
+	hamali_charges: function(frm) {
+		calculate_total_amount(frm);
+	},
+	detention_charges: function(frm) {
+		calculate_total_amount(frm);
+	},
+	bilty_charges: function(frm) {
+		calculate_total_amount(frm);
+	},
+	rto_charges: function(frm) {
+		calculate_total_amount(frm);
+	},
+	other_charges: function(frm) {
+		calculate_total_amount(frm);
+	}
+});
+
+frappe.ui.form.on("LR Item", {
+	amount: function(frm, cdt, cdn) {
+		calculate_basic_freight(frm);
+	},
+	items_remove: function(frm) {
+		calculate_basic_freight(frm);
+	}
+});
+
+function calculate_basic_freight(frm) {
+	let total_basic = 0;
+	if (frm.doc.items) {
+		frm.doc.items.forEach(function(item) {
+			total_basic += flt(item.amount);
+		});
+	}
+	frm.set_value("basic_freight", total_basic);
+	calculate_total_amount(frm);
+}
+
+function calculate_total_amount(frm) {
+	let total = flt(frm.doc.basic_freight) + 
+				flt(frm.doc.hamali_charges) + 
+				flt(frm.doc.detention_charges) + 
+				flt(frm.doc.bilty_charges) + 
+				flt(frm.doc.rto_charges) + 
+				flt(frm.doc.other_charges);
+	frm.set_value("total_amount", total);
+}
--- mytransport/mytransport/doctype/lorry_receipt/lorry_receipt.py
+++ mytransport/mytransport/doctype/lorry_receipt/lorry_receipt.py
@@ -1,17 +1,26 @@
-# Copyright (c) 2026, Shaily Sharma and contributors
+# Copyright (c) 2026, Ridhira Technologies and contributors
 # For license information, please see license.txt
 
 import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 class LorryReceipt(Document):
     def before_save(self):
+        # Calculate Total Amount from freight charges
+        self.total_amount = (
+            flt(self.basic_freight) +
+            flt(self.hamali_charges) +
+            flt(self.detention_charges) +
+            flt(self.bilty_charges) +
+            flt(self.rto_charges) +
+            flt(self.other_charges)
+        )
+
         if not self.paid_amount:
             self.paid_amount = 0
-        if not self.outstanding_amount and self.total_amount:
-            self.outstanding_amount = self.total_amount - self.paid_amount
-        elif self.total_amount:
-            self.outstanding_amount = self.total_amount - self.paid_amount
+            
+        self.outstanding_amount = self.total_amount - self.paid_amount
             
         if self.paid_amount == 0:
             self.receipt_status = "Unpaid"
